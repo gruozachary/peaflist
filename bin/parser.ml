331e8e00bd69c0ee8496298b93efbe3212a1c782
Thomas Zachary <thomaszachary06@proton.me>
2025-10-11 16:01:28 +0100

    feat(): Add initial grammar

--- bin/parser.ml
+++ bin/parser.ml
@@ -2,6 +2,31 @@ open! Base
 open! Sqc.Peasec
 open! Let_syntax
 
+(* Peaflist v1 grammar *)
+(*
+  Prog      ::= Expr ;
+
+  Expr      ::= Int
+              | Id
+              | Expr Expr                       function application
+              | "(" Expr ")"                    group
+              | "fun" Id "->" Expr              lamda
+              | "let" Id "=" Expr "in" Expr     binding
+              | "[" ListElems? "]"
+              | Expr BinOp Expr
+              ;
+
+  ListElems ::= Expr ("," Expr)* ;
+
+  BinOp     ::= "+" | "-" | "*" | "/" | "++" ;
+
+  Int       ::= Digit+ ;
+  Digit     ::= "0" | ... | "9" ;
+
+  Id        ::= Letter (Letter | Digit | "_")* ;
+  Letter    ::= "a" | ... "z" | "A" | ... | "Z" ;
+*)
+
 let id =
   let%map first = letter
   and rest = many (first_ok (first_ok letter digit) (char '_')) in
