b17634414b4fc7f97a6ee5aef8e37550167f367a
Thomas Zachary <thomaszachary06@proton.me>
2025-10-11 17:12:49 +0100

    feat(parsing): Add AST and parsing stub

--- bin/parser.ml
+++ bin/parser.ml
@@ -27,10 +27,46 @@ open! Let_syntax
   Letter    ::= "a" | ... "z" | "A" | ... | "Z" ;
 *)
 
-let id =
+module AST = struct
+  type nonrec int = int
+  type id = string
+  type bin_op = Plus | Sub | Mul | Div | Append
+
+  type expr =
+    | Int of int
+    | Id of id
+    | Apply of expr * expr
+    | Group of expr
+    | Lambda of id * expr
+    | Binding of id * expr * expr
+    | List of list_elems
+    | BinOp of expr * bin_op * expr
+
+  and list_elems = expr list
+end
+
+let expr : AST.expr t = return (AST.Int 5)
+
+and id : AST.id t =
   let%map first = letter
   and rest = many (first_ok (first_ok letter digit) (char '_')) in
   String.of_char_list (first :: rest)
+
+(* TODO: this will result on a horrible disaster if there are too many digits *)
+and int : AST.int t =
+  let%map first = digit and rest = many digit in
+  Int.of_string (String.of_char_list (first :: rest))
+
+and bin_op : AST.bin_op t =
+  match%map
+    choice [ symbol "+"; symbol "-"; symbol "*"; symbol "/"; symbol "++" ]
+  with
+  | "+" -> AST.Plus
+  | "-" -> AST.Sub
+  | "*" -> AST.Mul
+  | "/" -> AST.Div
+  | "++" -> AST.Append
+  | _ -> assert false
 
 include Sqc.Peasec
 include Sqc.Peasec.Let_syntax
