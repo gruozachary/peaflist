9f8b5c08b758237972ed01d6250f24d1cc03b2b2
Thomas Zachary <thomaszachary06@proton.me>
2025-10-11 13:10:02 +0100

    feat(parsing): Add some new combinators to parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,15 @@
 open! Base
-open Parser
+
+let parser : string Sqc.Peasec.t =
+  let open Parser in
+  let%map _ = symbol (char '(') and x = symbol id and _ = symbol (char ')') in
+  x
 
 let () =
   let open Stdio in
   match In_channel.input_line In_channel.stdin with
   | Some line -> (
-      match Sqc.Peasec.exec id line with Some s -> print_endline s | _ -> ())
+      match Sqc.Peasec.exec parser line with
+      | Some s -> print_endline s
+      | _ -> ())
   | _ -> ()
--- bin/parser.ml
+++ bin/parser.ml
@@ -5,8 +5,17 @@ open! Let_syntax
 let char c = satisfy (equal_char c)
 let letter = satisfy Char.is_alpha
 let digit = satisfy Char.is_digit
+let space = satisfy Char.is_whitespace
+let spaces = ignore_m (many space)
+
+let symbol p =
+  let%bind _ = spaces in
+  p
 
 let id =
   let%map first = letter
   and rest = many (first_ok (first_ok letter digit) (char '_')) in
   String.of_char_list (first :: rest)
+
+include Sqc.Peasec
+include Sqc.Peasec.Let_syntax
