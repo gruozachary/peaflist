822978bd5b6df60e9d6ed87346648be42b03ad29
Thomas Zachary <thomaszachary06@proton.me>
2025-10-17 15:42:43 +0100

    feat(parsing): Switch to record to allow rank-1 type in type Also add some new combinator

--- lib/peasec2.ml
+++ lib/peasec2.ml
@@ -1,63 +1,68 @@
 open! Base
 
-type ('a, 'r) t =
-  | P of
-      (string ->
-      ('a -> string -> unit -> 'r) ->
-      ('a -> unit -> 'r) ->
-      (string -> unit -> 'r) ->
-      (unit -> 'r) ->
-      'r)
-
-let unparser p inp cok eok cerr eerr =
-  match p with P f -> f inp cok eok cerr eerr
+type 'a t = {
+  unparser :
+    'r.
+    string ->
+    ('a -> string -> unit -> 'r) ->
+    ('a -> unit -> 'r) ->
+    (string -> unit -> 'r) ->
+    (unit -> 'r) ->
+    'r;
+}
 
 (* 'a - What this parser gives to it's next continuations *)
 (* 'r - What the next continuation returns *)
 
-let satisfy (f : char -> bool) : (char, 'r) t =
-  P
-    (fun inp cok _ _ eerr ->
-      if String.length inp > 0 && f inp.[0] then
-        cok inp.[0] (String.drop_prefix inp 1) ()
-      else eerr ())
+let satisfy (f : char -> bool) : char t =
+  {
+    unparser =
+      (fun inp cok _ _ eerr ->
+        if String.length inp > 0 && f inp.[0] then
+          cok inp.[0] (String.drop_prefix inp 1) ()
+        else eerr ());
+  }
 
 let exec p inp =
-  unparser p inp
+  p.unparser inp
     (fun x _ () -> Option.Some x)
     (fun x () -> Option.Some x)
     (fun _ () -> Option.None)
     (fun () -> Option.None)
 
 module Peasec_monad_basic = struct
-  type nonrec ('a, 'r) t = ('a, 'r) t
+  type nonrec 'a t = 'a t
 
-  let return x = P (fun _ _ eok _ _ -> eok x ())
+  let return x = { unparser = (fun _ _ eok _ _ -> eok x ()) }
 
   let bind p ~f =
-    P
-      (fun inp cok eok cerr eerr ->
-        unparser p inp
-          (fun x inp' () ->
-            unparser (f x) inp' cok (fun x -> cok x inp') cerr (cerr inp'))
-          (fun x () -> unparser (f x) inp cok eok cerr eerr)
-          cerr eerr)
+    {
+      unparser =
+        (fun inp cok eok cerr eerr ->
+          p.unparser inp
+            (fun x inp' () ->
+              (f x).unparser inp' cok (fun x -> cok x inp') cerr (cerr inp'))
+            (fun x () -> (f x).unparser inp cok eok cerr eerr)
+            cerr eerr);
+    }
 
   let map =
     `Custom
       (fun p ~f ->
-        P
-          (fun inp cok eok cerr eerr ->
-            unparser p inp
-              (fun x inp' -> cok (f x) inp')
-              (fun x -> eok (f x))
-              cerr eerr))
+        {
+          unparser =
+            (fun inp cok eok cerr eerr ->
+              p.unparser inp
+                (fun x inp' -> cok (f x) inp')
+                (fun x -> eok (f x))
+                cerr eerr);
+        })
 end
 
-module M = Monad.Make2 (Peasec_monad_basic)
+module M = Monad.Make (Peasec_monad_basic)
 
-module A = Applicative.Of_monad2 (struct
-  type nonrec ('a, 'r) t = ('a, 'r) t
+module A = Applicative.Of_monad (struct
+  type nonrec 'a t = 'a t
 
   include M
 end)
@@ -65,7 +70,21 @@ end)
 include M
 include A
 
+let empty = { unparser = (fun _ _ _ _ eerr -> eerr ()) }
+
 let ( <|> ) lp rp =
-  P
-    (fun inp cok eok cerr eerr ->
-      unparser lp inp cok eok cerr (fun () -> unparser rp inp cok eok cerr eerr))
+  {
+    unparser =
+      (fun inp cok eok cerr eerr ->
+        lp.unparser inp cok eok cerr (fun () ->
+            rp.unparser inp cok eok cerr eerr));
+  }
+
+let choice = List.fold ~init:empty ~f:( <|> )
+
+(* Combinators which are more string-related *)
+
+let char c = satisfy (equal_char c)
+let letter = satisfy Char.is_alpha
+let digit = satisfy Char.is_digit
+let ws = satisfy Char.is_whitespace
