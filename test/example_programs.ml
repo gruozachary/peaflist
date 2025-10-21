af23c853b7d0f98ea0e3e936d2d85cc541e52c4a
Thomas Zachary <thomaszachary06@proton.me>
2025-10-21 21:58:02 +0100

    feat(testing): Add two new program tests

--- test/example_programs.ml
+++ test/example_programs.ml
@@ -26,3 +26,63 @@ let val_declaration_crazy_whitespace =
             Ast.Lambda ("x", Ast.BinOp (Ast.Id "x", Ast.Mul, Ast.Id "x")) );
       ];
   }
+
+let maybe_declaration =
+  {
+    str = {|
+td ('a) maybe :=
+  | Just of 'a
+  | Nothing
+        |};
+    ast =
+      [
+        Ast.TypeDecl
+          ( "maybe",
+            [ "'a" ],
+            [ ("Just", Some (Ast.TyId "'a")); ("Nothing", None) ] );
+      ];
+  }
+
+let list_length =
+  {
+    str =
+      {|
+td ('a) list :=
+  | Cons of 'a * 'a list
+  | Nil
+
+vd length := fun l ->
+  match l with
+    | Nil       -> 0
+    | Cons x xs -> 1 + length xs
+|};
+    ast =
+      [
+        Ast.TypeDecl
+          ( "list",
+            [ "'a" ],
+            [
+              ( "Cons",
+                Some
+                  (Ast.TyProd
+                     (Ast.TyId "'a", Ast.TyApp (Ast.TyId "'a", Ast.TyId "list")))
+              );
+              ("Nil", None);
+            ] );
+        Ast.ValDecl
+          ( "length",
+            Ast.Lambda
+              ( "l",
+                Ast.Match
+                  ( Ast.Id "l",
+                    [
+                      (Ast.Id "Nil", Ast.Int 0);
+                      ( Ast.Apply
+                          (Ast.Apply (Ast.Id "Cons", Ast.Id "x"), Ast.Id "xs"),
+                        Ast.BinOp
+                          ( Ast.Int 1,
+                            Ast.Plus,
+                            Ast.Apply (Ast.Id "length", Ast.Id "xs") ) );
+                    ] ) ) );
+      ];
+  }
--- test/test_sqc.ml
+++ test/test_sqc.ml
@@ -4,3 +4,5 @@ open Example_programs
 [%%program_tests empty]
 [%%program_tests val_declaration]
 [%%program_tests val_declaration_crazy_whitespace]
+[%%program_tests maybe_declaration]
+[%%program_tests list_length]
