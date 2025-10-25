1974f37ce70991cc5d901fa18f1efd296b9c8816
Thomas Zachary <thomaszachary06@proton.me>
2025-10-25 12:51:12 +0100

    feat(typechecking): Add occurs function and add function for subst

--- lib/typechecking.ml
+++ lib/typechecking.ml
@@ -2,12 +2,17 @@ open! Base
 
 type alpha = string
 
+type concrete =
+  | Int
+  | Unit
+[@@deriving eq]
+
 type tau =
   | TVar of alpha
   | TFun of tau * tau
-  | TProd of tau * tau
-  | TApp of tau * tau
-  | TCon of [ `Int | `Unit ]
+  | TProd of tau list
+  | TApp of alpha * tau list
+  | TCon of concrete
 
 type sigma = alpha list * tau
 
@@ -24,6 +29,15 @@ end = struct
   let introduce env tid ty = Map.add_exn env ~key:tid ~data:ty
   let lookup env tid = Map.find env tid
 end
+
+let rec occurs (tvar : alpha) (t : tau) : bool =
+  match t with
+  | TVar x -> equal_string x tvar
+  | TFun (t'0, t'1) -> occurs tvar t'0 || occurs tvar t'1
+  | TProd ts -> List.exists ~f:(occurs tvar) ts
+  | TApp (tvar', ts) -> equal_string tvar tvar' || List.exists ~f:(occurs tvar) ts
+  | TCon _ -> false
+;;
 
 module State = struct
   type t = { mutable next : int }
@@ -47,9 +61,21 @@ module Subst = struct
        | Some t' -> t'
        | None -> t)
     | TFun (t'0, t'1) -> TFun (apply ~sub t'0, apply ~sub t'1)
-    | TProd (t'0, t'1) -> TProd (apply ~sub t'0, apply ~sub t'1)
-    | TApp (t'0, t'1) -> TApp (apply ~sub t'0, apply ~sub t'1)
+    | TProd t's -> TProd (List.map t's ~f:(apply ~sub))
+    | TApp (tvar, t's) -> TApp (tvar, List.map t's ~f:(apply ~sub))
     | TCon _ -> t
+  ;;
+
+  let add ~sub tvar t =
+    match t with
+    | TVar x ->
+      if equal_string x tvar
+      then Result.Ok sub
+      else Result.Ok (Map.add_exn sub ~key:tvar ~data:t)
+    | _ ->
+      if occurs tvar t
+      then Result.Error "Recursive type variable definiton"
+      else Result.Ok (Map.add_exn sub ~key:tvar ~data:t)
   ;;
 end
 
@@ -58,6 +84,7 @@ type error = string
 type ctx =
   { env : Gamma.t
   ; state : State.t
+  ; subst : Subst.t
   }
 
 let rec typecheck_expr ~(ctx : ctx) (e : Ast.expr) : (tau, error) Result.t =
@@ -67,16 +94,15 @@ let rec typecheck_expr ~(ctx : ctx) (e : Ast.expr) : (tau, error) Result.t =
   | Ast.Binding (_, _, _) -> Result.Error "Not implemented"
   | Ast.Group e -> typecheck_expr ~ctx e
   | Ast.Id x -> Result.of_option (Gamma.lookup ctx.env x) ~error:"Undefined variable"
-  | Ast.Int _ -> Result.Ok (TCon `Int)
+  | Ast.Int _ -> Result.Ok (TCon Int)
   | Ast.Lambda (_, _) -> Result.Error "Not implemented"
   | Ast.List _ -> Result.Error "Not implemented"
   | Ast.Match (_, _) -> Result.Error "Not implemented"
   | Ast.Tuple es ->
     (match es with
-     | [] -> Result.Ok (TCon `Unit)
-     | e :: es ->
+     | [] -> Result.Ok (TCon Unit)
+     | es ->
        let open Result.Let_syntax in
-       let%bind t = typecheck_expr ~ctx e in
        let%map ts = Result.all (List.map ~f:(typecheck_expr ~ctx) es) in
-       List.fold ~init:t ~f:(fun acc x -> TProd (acc, x)) ts)
+       TProd ts)
 ;;
