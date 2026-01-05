a4fecdc0e68d5c50f2fcfd9b6c0146a3849d85e8
Thomas Zachary <thomaszachary06@proton.me>
2026-01-05 15:06:27 +0000

    feat(anf): Add other trivial cases to conversion function

--- lib/mid/anf.ml
+++ lib/mid/anf.ml
@@ -23,21 +23,29 @@ module Expr = struct
   (* match will be done later *)
 
   let of_core_expr (ctx : Ctx.t) e =
+    let add_binding ~(e : compound t) ~t bindings =
+      let ident, _ =
+        Renamer.declare_and_fetch ctx.renamer ~heart:ctx.renamer_heart ~str:"v"
+      in
+      Var (ident, t), (ident, e) :: bindings
+    in
     let rec go e =
       match e with
       | Core.Expr.Int x -> Int x, []
       | Core.Expr.Id (ident, ty) -> Var (ident, ty), []
-      | Core.Expr.Constr _ -> _
-      | Core.Expr.Apply _ -> _
+      | Core.Expr.Constr (ctor_ident, es, t) ->
+        let es_a, bindings = List.map ~f:go es |> List.unzip in
+        List.concat bindings |> add_binding ~e:(Constr (ctor_ident, es_a, t)) ~t
+      | Core.Expr.Apply (e, e', t) ->
+        let e_a, bindings = go e
+        and e_a', bindings' = go e' in
+        List.append bindings bindings' |> add_binding ~e:(Apply (e_a, e_a', t)) ~t
       | Core.Expr.Lambda _ -> _
       | Core.Expr.Let _ -> _
       | Core.Expr.Match _ -> raise_s [%message "Match not currently implemented"]
       | Core.Expr.Tuple (es, t) ->
-        let es, bindings = List.map ~f:go es |> List.unzip in
-        let ident, _ =
-          Renamer.declare_and_fetch ctx.renamer ~heart:ctx.renamer_heart ~str:"v"
-        in
-        Var (ident, t), (ident, Tuple es) :: List.concat bindings
+        let es_a, bindings = List.map ~f:go es |> List.unzip in
+        List.concat bindings |> add_binding ~e:(Tuple es_a) ~t
     in
     let e, _ = go e in
     e
