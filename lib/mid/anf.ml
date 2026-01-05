d05916114f748cd4d84150550c583de429201837
Thomas Zachary <thomaszachary06@proton.me>
2026-01-05 14:48:25 +0000

    feat(anf): add start of anf conversion function

--- lib/mid/anf.ml
+++ lib/mid/anf.ml
@@ -21,4 +21,25 @@ module Expr = struct
     | Let : Var_ident.t * Scheme.t * 'any t * 'any t -> compound t
     | Tuple : atomic t List.t -> compound t
   (* match will be done later *)
+
+  let of_core_expr (ctx : Ctx.t) e =
+    let rec go e =
+      match e with
+      | Core.Expr.Int x -> Int x, []
+      | Core.Expr.Id (ident, ty) -> Var (ident, ty), []
+      | Core.Expr.Constr _ -> _
+      | Core.Expr.Apply _ -> _
+      | Core.Expr.Lambda _ -> _
+      | Core.Expr.Let _ -> _
+      | Core.Expr.Match _ -> raise_s [%message "Match not currently implemented"]
+      | Core.Expr.Tuple (es, t) ->
+        let es, bindings = List.map ~f:go es |> List.unzip in
+        let ident, _ =
+          Renamer.declare_and_fetch ctx.renamer ~heart:ctx.renamer_heart ~str:"v"
+        in
+        Var (ident, t), (ident, Tuple es) :: List.concat bindings
+    in
+    let e, _ = go e in
+    e
+  ;;
 end
