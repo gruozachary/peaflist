26293a988da76c59cd1fbe996ac2ee262ffdc828
Thomas Zachary <thomaszachary06@proton.me>
2026-01-07 20:01:59 +0000

    feat(ast): Move to using more adaptable AST representation

--- lib/lang/ast.ml
+++ lib/lang/ast.ml
@@ -1,58 +1,188 @@
-module Pat = struct
-  type ('vident, 'cident) t' =
-    | Int of int
-    | Ident of 'vident
-    | Tuple of ('vident, 'cident) t List.t
-    | Constr of 'cident * ('vident, 'cident) t List.t
+module type Meta = sig
+  type var_ident
+  type ctor_ident
+  type type_ident
+  type tvar
 
-  and ('vident, 'cident) t = { node : ('vident, 'cident) t' }
+  module Ext : sig
+    module Pat : sig
+      type for_int
+      type for_ident
+      type for_tuple
+      type for_constr
+    end
+
+    module Expr : sig
+      type for_int
+      type for_ident
+      type for_constr
+      type for_apply
+      type for_group
+      type for_lambda
+      type for_let
+      type for_match
+      type for_tuple
+      type for_binop
+    end
+
+    module Ty : sig
+      type for_var
+      type for_con
+      type for_prod
+      type for_fun
+    end
+
+    module Decl : sig
+      type for_val
+      type for_type
+    end
+
+    module Prog : sig
+      type for_prog
+    end
+  end
 end
 
-module Expr = struct
-  module Bin_op = struct
+module Make (M : Meta) = struct
+  module Pat = struct
     type t =
-      | Plus
-      | Sub
-      | Mul
-      | Div
+      | Int of int * M.Ext.Pat.for_int
+      | Ident of M.var_ident * M.Ext.Pat.for_ident
+      | Tuple of t list * M.Ext.Pat.for_tuple
+      | Constr of M.ctor_ident * t list * M.Ext.Pat.for_constr
   end
 
-  type ('vident, 'cident) t' =
-    | Int of int
-    | Ident of 'vident
-    | Constr of 'cident * ('vident, 'cident) t List.t
-    | Apply of ('vident, 'cident) t * ('vident, 'cident) t
-    | Group of ('vident, 'cident) t
-    | Lambda of 'vident * ('vident, 'cident) t
-    | Let of 'vident * ('vident, 'cident) t * ('vident, 'cident) t
-    | Match of
-        ('vident, 'cident) t * (('vident, 'cident) Pat.t * ('vident, 'cident) t) List.t
-    | Tuple of ('vident, 'cident) t List.t
-    | BinOp of ('vident, 'cident) t * Bin_op.t * ('vident, 'cident) t
+  module Expr = struct
+    module Bin_op = struct
+      type t =
+        | Plus
+        | Sub
+        | Mul
+        | Div
+    end
 
-  and ('vident, 'cident) t = { node : ('vident, 'cident) t' }
+    type t =
+      | Int of int * M.Ext.Expr.for_int
+      | Ident of M.var_ident * M.Ext.Expr.for_ident
+      | Constr of M.ctor_ident * t List.t * M.Ext.Expr.for_constr
+      | Apply of t * t * M.Ext.Expr.for_apply
+      | Group of t * M.Ext.Expr.for_group
+      | Lambda of M.var_ident * t * M.Ext.Expr.for_lambda
+      | Let of M.var_ident * t * t * M.Ext.Expr.for_let
+      | Match of t * (Pat.t * t) List.t * M.Ext.Expr.for_match
+      | Tuple of t List.t * M.Ext.Expr.for_tuple
+      | BinOp of t * Bin_op.t * t * M.Ext.Expr.for_binop
+  end
+
+  module Ty = struct
+    type t =
+      | Var of M.tvar * M.Ext.Ty.for_var
+      | Con of M.type_ident * t list * M.Ext.Ty.for_con
+      | Prod of t list * M.Ext.Ty.for_prod
+      | Fun of t * t * M.Ext.Ty.for_fun
+  end
+
+  module Decl = struct
+    type t =
+      | Val of M.var_ident * Expr.t * M.Ext.Decl.for_val
+      | Type of
+          M.type_ident
+          * M.tvar list
+          * (M.ctor_ident * Ty.t option) list
+          * M.Ext.Decl.for_type
+  end
+
+  module Prog = struct
+    type t = Decl.t list * M.Ext.Prog.for_prog
+  end
 end
 
-module Ty = struct
-  type ('tvar, 'tident) t' =
-    | Var of 'tvar
-    | Con of 'tident * ('tvar, 'tident) t List.t
-    | Prod of ('tvar, 'tident) t List.t
-    | Fun of ('tvar, 'tident) t * ('tvar, 'tident) t
+module Parsed = Make (struct
+    type var_ident = string
+    type ctor_ident = string
+    type type_ident = string
+    type tvar = string
 
-  and ('tvar, 'tident) t = { node : ('tvar, 'tident) t' }
-end
+    module Ext = struct
+      module Pat = struct
+        type for_int = unit
+        type for_ident = unit
+        type for_tuple = unit
+        type for_constr = unit
+      end
 
-module Decl = struct
-  type ('vident, 'cident, 'tvar, 'tident) t' =
-    | Val of 'vident * ('vident, 'cident) Expr.t
-    | Type of 'tident * 'tvar List.t * ('cident * ('tvar, 'tident) Ty.t Option.t) List.t
+      module Expr = struct
+        type for_int = unit
+        type for_ident = unit
+        type for_constr = unit
+        type for_apply = unit
+        type for_group = unit
+        type for_lambda = unit
+        type for_let = unit
+        type for_match = unit
+        type for_tuple = unit
+        type for_binop = unit
+      end
 
-  and ('vident, 'cident, 'tvar, 'tident) t =
-    { node : ('vident, 'cident, 'tvar, 'tident) t' }
-end
+      module Ty = struct
+        type for_var = unit
+        type for_con = unit
+        type for_prod = unit
+        type for_fun = unit
+      end
 
-module Prog = struct
-  type ('vident, 'cident, 'tvar, 'tident) t' =
-    ('vident, 'cident, 'tvar, 'tident) Decl.t List.t
-end
+      module Decl = struct
+        type for_val = unit
+        type for_type = unit
+      end
+
+      module Prog = struct
+        type for_prog = unit
+      end
+    end
+  end)
+
+module Renamed = Make (struct
+    type var_ident = Var_ident.t
+    type ctor_ident = Constr_ident.t
+    type type_ident = Type_ident.t
+    type tvar = int
+
+    module Ext = struct
+      module Pat = struct
+        type for_int = unit
+        type for_ident = unit
+        type for_tuple = unit
+        type for_constr = unit
+      end
+
+      module Expr = struct
+        type for_int = unit
+        type for_ident = unit
+        type for_constr = unit
+        type for_apply = unit
+        type for_group = unit
+        type for_lambda = unit
+        type for_let = unit
+        type for_match = unit
+        type for_tuple = unit
+        type for_binop = unit
+      end
+
+      module Ty = struct
+        type for_var = unit
+        type for_con = unit
+        type for_prod = unit
+        type for_fun = unit
+      end
+
+      module Decl = struct
+        type for_val = unit
+        type for_type = unit
+      end
+
+      module Prog = struct
+        type for_prog = unit
+      end
+    end
+  end)
