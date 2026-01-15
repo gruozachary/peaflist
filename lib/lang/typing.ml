4b4b1906594bd25944e2edd030fd5d7404175d6e
Thomas Zachary <thomaszachary06@proton.me>
2026-01-15 20:58:49 +0000

    feat(typing): Add sexp conversion functions to type and scheme

--- lib/lang/typing.ml
+++ lib/lang/typing.ml
@@ -5,6 +5,7 @@ module Make_var () : sig
 
   val zero : t
   val succ : t -> t
+  val sexp_of_t : t -> Sexp.t
 
   include Comparable.S with type t := t
 end = struct
@@ -39,6 +40,22 @@ module Type = struct
     | Unbound of Uni_var.t
     | Link of phantom_ununified t
 
+  let rec sexp_of_t : type a. a t -> Sexp.t = function
+    | Gen gen_var -> Sexp.List [ Sexp.Atom "Gen"; Gen_var.sexp_of_t gen_var ]
+    | Uni { contents = Unbound uni_var } ->
+      Sexp.List [ Sexp.Atom "Unbound"; Uni_var.sexp_of_t uni_var ]
+    | Uni { contents = Link ty } -> Sexp.List [ Sexp.Atom "Link"; sexp_of_t ty ]
+    | Fun (ty_arg, ty_res) ->
+      Sexp.List [ Sexp.Atom "Fun"; sexp_of_t ty_arg; sexp_of_t ty_res ]
+    | Prod tys -> Sexp.List [ Sexp.Atom "Prod"; Sexp.List (List.map ~f:sexp_of_t tys) ]
+    | Con (ident, tys) ->
+      Sexp.List
+        [ Sexp.Atom "Con"
+        ; Type_ident.sexp_of_t ident
+        ; Sexp.List (List.map ~f:sexp_of_t tys)
+        ]
+  ;;
+
   type unified_t = phantom_unified t
   type ununified_t = phantom_ununified t
 end
@@ -48,4 +65,13 @@ module Scheme = struct
   type any_t = Any : 'any t -> any_t
   type unified_t = Type.phantom_unified t
   type ununified_t = Type.phantom_ununified t
+
+  let sexp_of_t : type a. a t -> Sexp.t =
+    fun (Forall (gen_vars, ty)) ->
+    Sexp.List
+      [ Sexp.Atom "Forall"
+      ; Sexp.List (List.map ~f:Gen_var.sexp_of_t gen_vars)
+      ; Type.sexp_of_t ty
+      ]
+  ;;
 end
