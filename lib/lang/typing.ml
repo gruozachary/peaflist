25999384e073ce7294404be424f1b3a5c9749bef
Thomas Zachary <thomaszachary06@proton.me>
2026-01-16 15:46:27 +0000

    feat(types): Add pretty printing of types and schemes

--- lib/lang/typing.ml
+++ lib/lang/typing.ml
@@ -25,6 +25,27 @@ end
 module Gen_var = Make_var ()
 module Uni_var = Make_var ()
 
+let label_gen_vars
+  :  (Gen_var.t, Gen_var.comparator_witness) Set.t
+  -> (Gen_var.t, string, Gen_var.comparator_witness) Map.t
+  =
+  fun gen_vars ->
+  let _, mapping =
+    Set.fold
+      gen_vars
+      ~init:(97, Map.empty (module Gen_var))
+      ~f:(fun (i, mapping) gen_var ->
+        ( i + 1
+        , Option.value_map (Char.of_int i) ~default:mapping ~f:(fun c ->
+            Map.set mapping ~key:gen_var ~data:(String.of_char c)) ))
+  in
+  mapping
+;;
+
+let gen_var_to_string_pretty mapping gen_var =
+  "'" ^ (Map.find mapping gen_var |> Option.value ~default:(Gen_var.to_string gen_var))
+;;
+
 module Type = struct
   type phantom_ununified
   type phantom_unified
@@ -69,15 +90,30 @@ module Type = struct
     | Con _ -> 3
   ;;
 
-  let rec to_string : type a. a t -> string =
-    fun ty ->
+  let rec gen_vars_of_ty : type a. a t -> (Gen_var.t, Gen_var.comparator_witness) Set.t =
+    function
+    | Gen gen_var -> Set.singleton (module Gen_var) gen_var
+    | Uni _ -> Set.empty (module Gen_var)
+    | Fun (ty_arg, ty_res) -> Set.union (gen_vars_of_ty ty_arg) (gen_vars_of_ty ty_res)
+    | Prod tys -> List.map ~f:gen_vars_of_ty tys |> Set.union_list (module Gen_var)
+    | Con (_, tys) -> List.map ~f:gen_vars_of_ty tys |> Set.union_list (module Gen_var)
+  ;;
+
+  let rec to_string
+    : type a.
+      ?mapping:(Gen_var.t, string, Gen_var.comparator_witness) Map.t -> a t -> string
+    =
+    fun ?mapping ty ->
+    let mapping = Option.value mapping ~default:(gen_vars_of_ty ty |> label_gen_vars) in
     let go ty' =
-      if prec ty' < prec ty then "(" ^ to_string ty' ^ ")" else to_string ty'
+      if prec ty' < prec ty
+      then "(" ^ to_string ~mapping ty' ^ ")"
+      else to_string ~mapping ty'
     in
     match ty with
     | Uni { contents = Unbound uni_var } -> "unbound" ^ Uni_var.to_string uni_var
     | Uni { contents = Link ty } -> "link" ^ go ty
-    | Gen gen_var -> Gen_var.to_string gen_var
+    | Gen gen_var -> gen_var_to_string_pretty mapping gen_var
     | Fun (t, t') -> go t ^ " -> " ^ go t'
     | Prod ts ->
       List.map ~f:go ts
@@ -110,12 +146,18 @@ module Scheme = struct
       ]
   ;;
 
-  let to_string : type a. a t -> string =
-    fun (Forall (qs, t)) ->
-    (List.map qs ~f:Gen_var.to_string
+  let to_string
+    : type a.
+      ?mapping:(Gen_var.t, string, Gen_var.comparator_witness) Map.t -> a t -> string
+    =
+    fun ?mapping (Forall (qs, ty)) ->
+    let mapping =
+      Option.value mapping ~default:(Type.gen_vars_of_ty ty |> label_gen_vars)
+    in
+    (List.map qs ~f:(fun gen_var -> gen_var_to_string_pretty mapping gen_var)
      |> List.intersperse ~sep:" "
      |> List.fold ~init:"forall " ~f:String.append)
     ^ " . "
-    ^ Type.to_string t
+    ^ Type.to_string ~mapping ty
   ;;
 end
