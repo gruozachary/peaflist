4369a3a1c2a73737b37ddc944bd65c2cf214ec57
Thomas Zachary <thomaszachary06@proton.me>
2026-01-16 22:40:54 +0000

    refactor(ast): Split up AST into composable chunks

--- lib/lang/ast.ml
+++ lib/lang/ast.ml
@@ -2,13 +2,123 @@ open! Base
 
 type void = |
 
-module type META = sig
-  type var_ident
-  type ctor_ident
-  type type_ident
-  type tvar
+module Make_pat (M : sig
+    type var_ident
+    type ctor_ident
+    type for_int
+    type for_ident
+    type for_tuple
+    type for_constr
+  end) =
+struct
+  type t =
+    | Int of int * M.for_int
+    | Ident of M.var_ident * M.for_ident
+    | Tuple of t list * M.for_tuple
+    | Constr of M.ctor_ident * t list * M.for_constr
+end
 
-  module Ext : sig
+module Make_expr (M : sig
+    module Pat : sig
+      type t
+    end
+
+    type var_ident
+    type ctor_ident
+    type for_int
+    type for_ident
+    type for_constr
+    type for_apply
+    type for_group
+    type for_lambda
+    type for_let
+    type for_match
+    type for_tuple
+    type for_binop
+  end) =
+struct
+  module Bin_op = struct
+    type t =
+      [ `Plus
+      | `Mul
+      | `Sub
+      | `Div
+      ]
+  end
+
+  type t =
+    | Int of int * M.for_int
+    | Ident of M.var_ident * M.for_ident
+    | Constr of M.ctor_ident * t List.t * M.for_constr
+    | Apply of t * t * M.for_apply
+    | Group of t * M.for_group
+    | Lambda of M.var_ident * t * M.for_lambda
+    | Let of M.var_ident * t * t * M.for_let
+    | Match of t * (M.Pat.t * t) List.t * M.for_match
+    | Tuple of t List.t * M.for_tuple
+    | BinOp of t * Bin_op.t * t * M.for_binop
+end
+
+module Make_ty (M : sig
+    type type_ident
+    type tvar
+    type for_var
+    type for_con
+    type for_prod
+    type for_fun
+  end) =
+struct
+  type t =
+    | Var of M.tvar * M.for_var
+    | Con of M.type_ident * t list * M.for_con
+    | Prod of t list * M.for_prod
+    | Fun of t * t * M.for_fun
+end
+
+module Make_decl (M : sig
+    module Expr : sig
+      type t
+    end
+
+    module Ty : sig
+      type t
+    end
+
+    type var_ident
+    type type_ident
+    type tvar
+    type ctor_ident
+    type for_val
+    type for_type
+    type for_ctor
+  end) =
+struct
+  type t =
+    | Val of M.var_ident * M.Expr.t * M.for_val
+    | Type of
+        M.type_ident
+        * M.tvar list
+        * (M.ctor_ident * M.Ty.t option * M.for_ctor) list
+        * M.for_type
+end
+
+module Make_prog (M : sig
+    module Decl : sig
+      type t
+    end
+
+    type for_decls
+  end) =
+struct
+  type t = Decls of M.Decl.t list * M.for_decls
+end
+
+module Make (M : sig
+    type var_ident
+    type ctor_ident
+    type type_ident
+    type tvar
+
     module Pat : sig
       type for_int
       type for_ident
@@ -43,64 +153,49 @@ module type META = sig
     end
 
     module Prog : sig
-      type for_prog
+      type for_decls
     end
-  end
-end
+  end) =
+struct
+  module Pat = Make_pat (struct
+      type var_ident = M.var_ident
+      type ctor_ident = M.ctor_ident
 
-module Make (M : META) = struct
-  module Pat = struct
-    type t =
-      | Int of int * M.Ext.Pat.for_int
-      | Ident of M.var_ident * M.Ext.Pat.for_ident
-      | Tuple of t list * M.Ext.Pat.for_tuple
-      | Constr of M.ctor_ident * t list * M.Ext.Pat.for_constr
-  end
+      include M.Pat
+    end)
 
-  module Expr = struct
-    module Bin_op = struct
-      type t =
-        [ `Plus
-        | `Mul
-        | `Sub
-        | `Div
-        ]
-    end
+  module Expr = Make_expr (struct
+      module Pat = Pat
 
-    type t =
-      | Int of int * M.Ext.Expr.for_int
-      | Ident of M.var_ident * M.Ext.Expr.for_ident
-      | Constr of M.ctor_ident * t List.t * M.Ext.Expr.for_constr
-      | Apply of t * t * M.Ext.Expr.for_apply
-      | Group of t * M.Ext.Expr.for_group
-      | Lambda of M.var_ident * t * M.Ext.Expr.for_lambda
-      | Let of M.var_ident * t * t * M.Ext.Expr.for_let
-      | Match of t * (Pat.t * t) List.t * M.Ext.Expr.for_match
-      | Tuple of t List.t * M.Ext.Expr.for_tuple
-      | BinOp of t * Bin_op.t * t * M.Ext.Expr.for_binop
-  end
+      type var_ident = M.var_ident
+      type ctor_ident = M.ctor_ident
 
-  module Ty = struct
-    type t =
-      | Var of M.tvar * M.Ext.Ty.for_var
-      | Con of M.type_ident * t list * M.Ext.Ty.for_con
-      | Prod of t list * M.Ext.Ty.for_prod
-      | Fun of t * t * M.Ext.Ty.for_fun
-  end
+      include M.Expr
+    end)
 
-  module Decl = struct
-    type t =
-      | Val of M.var_ident * Expr.t * M.Ext.Decl.for_val
-      | Type of
-          M.type_ident
-          * M.tvar list
-          * (M.ctor_ident * Ty.t option * M.Ext.Decl.for_ctor) list
-          * M.Ext.Decl.for_type
-  end
+  module Ty = Make_ty (struct
+      type type_ident = M.type_ident
+      type tvar = M.tvar
 
-  module Prog = struct
-    type t = Decls of Decl.t list * M.Ext.Prog.for_prog
-  end
+      include M.Ty
+    end)
+
+  module Decl = Make_decl (struct
+      module Expr = Expr
+      module Ty = Ty
+
+      type var_ident = M.var_ident
+      type type_ident = M.type_ident
+      type ctor_ident = M.ctor_ident
+      type tvar = M.tvar
+
+      include M.Decl
+    end)
+
+  module Prog = Make_prog (struct
+      module Decl = Decl
+      include M.Prog
+    end)
 end
 
 let equal_pairs equal xs ys =
