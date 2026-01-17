a1d174b10d506c02e2a154baee2b76e44c1a0113
Thomas Zachary <thomaszachary06@proton.me>
2026-01-17 18:00:57 +0000

    feat(match-compilation): Add trivial conversion functions

--- lib/lang/match_compile.ml
+++ lib/lang/match_compile.ml
@@ -25,7 +25,7 @@ module Ast = struct
       type var_ident = Var_ident.t
       type type_ident = Type_ident.t
       type ctor_ident = Constr_ident.t
-      type tvar = int
+      type tvar = Gen_var.t
       type for_val = Scheme.unified_t
       type for_type = Core_ast.Unified.type_data
       type for_ctor = Core_ast.Unified.ctor_data
@@ -37,3 +37,29 @@ module Ast = struct
       type for_decls = unit
     end)
 end
+
+open Ast
+open Result.Let_syntax
+
+let convert_expr : Core_ast.Unified.Expr.t -> (Expr.t, string) Result.t = _
+
+let convert_decl : Core_ast.Unified.Decl.t -> (Decl.t, string) Result.t = function
+  | Core_ast.Unified.Decl.Val (ident, expr, scheme) ->
+    let%map expr = convert_expr expr in
+    Decl.Val (ident, expr, scheme)
+  | Core_ast.Unified.Decl.Type (ident, gen_vars, ctors, type_data) ->
+    return (Decl.Type (ident, gen_vars, ctors, type_data))
+;;
+
+let convert_prog : Core_ast.Unified.Prog.t -> (Prog.t, string) Result.t =
+  fun (Core_ast.Unified.Prog.Decls (decls, ())) ->
+  let%map decls =
+    List.fold_result
+      ~init:[]
+      ~f:(fun acc decl ->
+        let%map decl = convert_decl decl in
+        decl :: acc)
+      decls
+  in
+  Prog.Decls (decls, ())
+;;
